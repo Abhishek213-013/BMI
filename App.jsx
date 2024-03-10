74ed23adc45db1515d0874311ede1e93e253b3aa
Abhishek213-013 <141157999+Abhishek213-013@users.noreply.github.com>
2024-03-10 23:20:30 +0600

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import BMI from "./Components/BMI";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="Parent-container">
+        <BMI />
+      </div>
+    </div>
+  );
+}
+
+export default App;
